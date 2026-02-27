9b0bef858a27acc68161b455303ba2877763640c
AlinaLesovec <148266535+AlinaLesovec@users.noreply.github.com>
2026-02-27 05:25:19 +0300

    Create setting.py

---
+++ setting.py
@@ -0,0 +1,31 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG") == "True"
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "news",
+]
+
+TEMPLATES[0]["DIRS"] = [BASE_DIR / "templates"]
